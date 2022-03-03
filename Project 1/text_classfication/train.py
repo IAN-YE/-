feb01ff54b1b35d32a9259e253ea29cde8d0c91c
IAN-YE <496013786@qq.com>
2022-03-03 19:39:39 +0800

    update

--- Project 1/text_classfication/train.py
+++ Project 1/text_classfication/train.py
@@ -1,28 +1,34 @@
+import torchvision.utils
+
 import process
 import MLP
 import torch.utils.data as Data
 import numpy as np
 from sklearn.model_selection import train_test_split
+from torch.utils.tensorboard import SummaryWriter
 import torch
 import torch.nn as nn
 from torch.optim import SGD,Adam
 import seaborn as sns
 import hiddenlayer as hl
+import torchsummary
 from sklearn.metrics import accuracy_score
 
-USE_GPU = True
+#################################
+#Checking GPU is used
+#################################
+train_on_gpu = torch.cuda.is_available()
 
-dtype = torch.float32 # we will be using float throughout this tutorial
-
-if USE_GPU and torch.cuda.is_available():
-    device = torch.device('cuda')
+if not train_on_gpu:
+    print('CUDA is not available. Training on CPU')
 else:
-    device = torch.device('cpu')
+    print('CUDA is available. Training on GPU')
+
+device = torch.device("cuda:0" if train_on_gpu else "cpu")
+print('using device:', device)
 
 # Constant to control how frequently we print train loss
 print_every = 100
-
-print('using device:', device)
 
 x_train, y_train, x_to_test = process.process()
 
@@ -33,7 +39,9 @@ y_train_t = torch.from_numpy(y_train.astype(np.int64))
 X_test_t = torch.from_numpy(X_test.astype(np.float32))
 y_test_t = torch.from_numpy(y_test.astype(np.int64))
 
-train_data = Data.TensorDataset(X_train_t,y_train_t)
+# torchsummary.summary(X_train_t, input_size=(3,128,128), device='cuda')
+
+train_data = Data.TensorDataset(X_train_t, y_train_t)
 
 train_loader = Data.DataLoader(
     dataset=train_data,
@@ -52,6 +60,13 @@ history1 = hl.History()
 canvas1 = hl.Canvas()
 print_step = 25
 
+tb = SummaryWriter()
+text, labels = next(iter(train_loader))
+grid = torchvision.utils.make_grid(text)
+
+tb.add_image('texts', grid)
+tb.add_graph(mlpt, text)
+
 for epoch in range(15):
     for step, (b_x, b_y) in enumerate(train_loader):
         _, _, output = mlpt(b_x)
@@ -67,7 +82,8 @@ for epoch in range(15):
         ## 为history添加epoch,损失和精度
         history1.log(niter, train_loss=train_loss, test_accuracy=test_accuracy)
         print("epoch:{},train_loss:{},test_accuracy:{}".format(epoch, train_loss, test_accuracy))
-
+        tb.add_scalar('Loss', train_loss, epoch)
+        tb.add_scalar('Accuracy', test_accuracy, epoch)
         # with canvas1:
         #     canvas1.draw_plot(history1['train_loss'])
         #     canvas1.draw_plot(history1['test_accuracy'])
