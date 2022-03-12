c4bbcf76036f5726e72435184bd404f28ba2df1e
IAN-YE <10195101478@stu.ecnu.edu.cn>
2022-03-12 14:05:34 +0800

    update

--- Project 1/text_classfication/train.py
+++ Project 1/text_classfication/train.py
@@ -11,6 +11,61 @@ from torch.optim import SGD,Adam
 import seaborn as sns
 from sklearn.metrics import accuracy_score
 
+
+def train(x_train, y_train):
+    X_train, X_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=123)
+
+    X_train_t = torch.from_numpy(X_train.astype(np.float32))
+    y_train_t = torch.from_numpy(y_train.astype(np.int64))
+    X_test_t = torch.from_numpy(X_test.astype(np.float32))
+    y_test_t = torch.from_numpy(y_test.astype(np.int64))
+
+    train_data = Data.TensorDataset(X_train_t, y_train_t)
+    train_loader = Data.DataLoader(
+        dataset=train_data,
+        batch_size=64,
+        shuffle=True,
+    )
+
+    mlpt = MLP.MLP()
+    paras = list(mlpt.parameters())
+
+    #params
+    optimizer = torch.optim.Adam(mlpt.parameters(), lr=0.005)
+    loss_func = nn.CrossEntropyLoss()
+
+    #set tensorboard
+    tb = SummaryWriter()
+    images, labels = next(iter(train_loader))
+    grid = torchvision.utils.make_grid(images)
+    tb.add_image('texts', grid)
+    tb.add_graph(mlpt, images)
+
+    for epoch in range(10):
+        train_loss_e = []
+        train_accuracy_e = []
+        test_accuracy_e = []
+        for step, (b_x, b_y) in enumerate(train_loader):
+            output = mlpt(b_x)
+            train_loss = loss_func(output, b_y)
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+            train_output = mlpt(X_train_t)
+            _, pre_train = torch.max(train_output, 1)
+            output = mlpt(X_test_t)
+            _, pre_lab = torch.max(output, 1)
+            #print(pre_lab)
+            train_accuracy = accuracy_score(y_train_t, pre_train)
+            test_accuracy = accuracy_score(y_test_t, pre_lab)
+            print(type(train_accuracy))
+
+        # print("epoch:{},train_loss:{},train_accuracy{},test_accuracy:{}".format(epoch, (np.array(train_loss_e)).mean(), (np.array(train_accuracy_e)).mean(),                                                                            test_accuracy))
+        # tb.add_scalar('Loss', (np.array(train_loss_e)).mean(), epoch)
+        # tb.add_scalar('Accuracy', (np.array(test_accuracy)).mean(), epoch)
+    return mlpt
+
+
 #Checking GPU is used
 train_on_gpu = torch.cuda.is_available()
 
@@ -22,63 +77,11 @@ else:
 device = torch.device("cuda:0" if train_on_gpu else "cpu")
 print('using device:', device)
 
-# Constant to control how frequently we print train loss
-print_every = 100
-
 x_train, y_train, x_to_test = process.process()
-
-X_train, X_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=123)
-
-X_train_t = torch.from_numpy(X_train.astype(np.float32))
-y_train_t = torch.from_numpy(y_train.astype(np.int64))
-X_test_t = torch.from_numpy(X_test.astype(np.float32))
-y_test_t = torch.from_numpy(y_test.astype(np.int64))
 
 X_to_test = torch.from_numpy(x_to_test.astype(np.float32))
 
-# torchsummary.summary(X_train_t, input_size=(3,128,128), device='cuda')
-
-train_data = Data.TensorDataset(X_train_t, y_train_t)
-train_loader = Data.DataLoader(
-    dataset=train_data,
-    batch_size=32,
-    shuffle=True,
-)
-
-mlpt = MLP.MLP()
-paras = list(mlpt.parameters())
-
-optimizer = torch.optim.Adam(mlpt.parameters(), lr=0.005)
-loss_func = nn.CrossEntropyLoss()
-
-tb = SummaryWriter()
-images, labels = next(iter(train_loader))
-grid = torchvision.utils.make_grid(images)
-
-tb.add_image('texts', grid)
-tb.add_graph(mlpt, images)
-
-for epoch in range(10):
-    train_loss = 0
-    test_accuracy = 0
-    for step, (b_x, b_y) in enumerate(train_loader):
-        output = mlpt(b_x)
-        train_loss = loss_func(output, b_y)
-        optimizer.zero_grad()
-        train_loss.backward()
-        optimizer.step()
-        train_output = mlpt(X_train_t)
-        _, pre_train = torch.max(train_output, 1)
-        output = mlpt(X_test_t)
-        _, pre_lab = torch.max(output, 1)
-        print(pre_lab)
-        train_accuracy = accuracy_score(y_train_t,pre_train)
-        test_accuracy = accuracy_score(y_test_t, pre_lab)
-        print("epoch:{},train_loss:{},train_accuracy{},test_accuracy:{}".format(epoch, train_loss, train_accuracy, test_accuracy))
-        tb.add_scalar('Loss', train_loss, step)
-        tb.add_scalar('Accuracy', test_accuracy, step)
-
-
+mlpt = train(x_train,y_train)
 y_pre = mlpt(X_to_test)
 _, pre_lab = torch.max(y_pre, 1)
 pre_lab = pre_lab.tolist()
